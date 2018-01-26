319cff7986804e078ca5b861d424d4d7c8add34a
hangzou <1102639563@qq.com>
2018-01-26 17:34:31 +0800

    Update ajax.js
    token

--- ajax.js
+++ ajax.js
@@ -2,12 +2,26 @@ import { Message } from 'element-ui'
 import axios from 'axios'
 import qs from 'qs'
 import router from '../router'
+import { getToken } from 'common'
 
 const service = axios.create({
     baseURL: process.env.MOCK_URL,
     timeout: 60000
     // withCredentials: true
 });
+
+service.interceptors.request.use(config => {
+      // Do something before request is sent
+      if (store.getters.token) {
+        config.headers['X-Token'] = getToken() // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+      }
+      return config
+}, error => {
+      // Do something with request error
+      console.log(error) // for debug
+      Promise.reject(error)
+})
+
 
 service.interceptors.response.use(function(response) {
     if (response.headers.loginstate === 'expired') router.push({ path: '/login' })
