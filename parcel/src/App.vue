1e1b8c432d30bda0bd32141febbda2d3711ada2b
zouhang <1102639563@qq.com>
2018-01-30 17:32:26 +0800

    vue-parcel

---
+++ parcel/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app">
+        <h1>{{message}}</h1>
+        <div>666</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            message: 'hello world'
+        }
+    }
+}
+</script>
+<style>
+    .app {
+        color: #56b983;
+        display: flex;
+    }
+</style>
