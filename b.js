3744e8e72691b4826ab531bb1ca75d7b27c40e3b
zouhang <1102639563@qq.com>
2018-04-28 16:40:53 +0800

    observre

--- b.js
+++ b.js
@@ -34,3 +34,51 @@ export function debounce(func, delay) {
         }, delay)
     }
 }
+//
+
+let pubsub = {}
+
+function Observer(q) {
+    let topics = {}
+    let subUid = -1
+
+    // 发布方法
+    q.publish = function (topic, args) {
+        if (!topics[topic]) return false
+
+        setTimeout(() => {
+            let subscribers = topics[topic]
+            let len = subscribers? subscribers.length : 0
+            while (len--) {
+                subscribers[len].fn(topic, args)
+            }
+        }, 0);
+    }
+
+    // 订阅方法
+    q.subscribe = function (topic, fn) {
+        if (!topics[topic]) topics[topic] = []
+
+        let token = (++subUid).toString()
+
+        topics[topic].push({
+            token,
+            fn
+        })
+        return token
+    }
+
+    q.unsubscribe = function (token) {
+        for(let m in topics) {
+            if (topics[m]) {
+                for (let i = 0; i < topics[m].length; i++) {
+                    if (topics[m][i].token === token) {
+                        topics[m].splice(i, 1)
+                        return token
+                    }
+                }
+            }
+        }
+        return false
+    }
+}
