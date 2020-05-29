de4fd0672d240ccfe1ef0f35d9ca3ac55970c9aa
zouhang <zouhang@didiglobal.com>
2020-05-29 09:54:19 +0800

    :zap: 代码优化

--- urls.ts
+++ urls.ts
@@ -2,7 +2,7 @@ const url = " http://httpbin.org/delay/3"
 const urls: string[] = Array(10).fill(url)
 
 function fetchUrls(urls: string[], max: number) {
-  const finished = []
+  let key = max
   const urlsMap = urls.map((url, index) => ({
     async start() {
       const url = this.url
@@ -10,18 +10,16 @@ function fetchUrls(urls: string[], max: number) {
         method: 'GET',
       })
       this.done = true
-      finished.push(this)
-      window.postMessage({}, '/')
+      const cb = () => {
+        urlsMap[key] && urlsMap[key].start()
+        key++
+      }
+      cb()
     },
     done: false,
     url,
     index,
   }))
-
-  window.addEventListener('message', data => {
-    const nextIndex = finished.length + max - 1
-    urlsMap[nextIndex] && urlsMap[nextIndex].start()
-  })
 
   const run = () => {
     Promise.all(urlsMap.slice(0, max).map(u => u.start()))
