4802081b45ebc94b060965139d9f9c06a14ba81b
zouhang <zouhang@didiglobal.com>
2020-06-18 10:11:43 +0800

    :zap: shell example

--- bash/script.sh
+++ bash/script.sh
@@ -1,2 +1,11 @@
-read -p "Enter one or more values > "
-echo "REPLY = '$REPLY'"
+echo -n "输入一个1到3之间的数字（包含两端）> "
+read character
+case $character in
+  1 ) echo 1
+    ;;
+  2 ) echo 2
+    ;;
+  3 ) echo 3
+    ;;
+  * ) echo 输入不符合要求
+esac
